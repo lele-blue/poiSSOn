c3c8fb23d978106674ac4702074377eb8c73f297
Lele Eichelsdörfer <git@mysi-lea.xyz>
2024-09-19 00:08:33 +0200

    add support for multiple adresses in x-forwarded-for

--- main/middleware.py
+++ main/middleware.py
@@ -2,6 +2,6 @@ from django.conf import settings
 
 def reverse_proxy(get_response):
     def process_request(request):
-        request.META['REMOTE_ADDR'] = request.META.get('HTTP_X_FORWARDED_FOR').split(",")[0].trim() if not settings.DEBUG else '127.0.0.1'
+        request.META['REMOTE_ADDR'] = request.META.get('HTTP_X_FORWARDED_FOR').split(",")[0].strip() if not settings.DEBUG else '127.0.0.1'
         return get_response(request)
     return process_request
