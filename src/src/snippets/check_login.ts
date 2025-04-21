b4b2d2319752470906240ec928a3ed212f6ee562
Lele Eichelsdörfer <git@mysi-lea.xyz>
2025-04-21 17:08:14 +0200

    Fix ifinite reload bug

--- src/src/snippets/check_login.ts
+++ src/src/snippets/check_login.ts
@@ -3,8 +3,10 @@ import {currentUser} from "../state/currentUser";
 export async function check_login() {
     const res = await fetch("/auth/api/alt_ping");
     if (res.status !== 200) {
-        location.href = "/auth";
+        if (location.pathname !== "/auth") location.href = "/auth";
     }
-    currentUser.set(await res.json())
+    else {
+        currentUser.set(await res.json())
+    }
 }
 
